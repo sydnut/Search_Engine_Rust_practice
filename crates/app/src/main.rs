ff80117f727c8a6f69ed0dab38c9bbac5117195b
fengzhaoyang.8 <fengzhaoyang.8@jd.com>
2026-08-13 18:31:40 +0800

    feat: #2 完善http服务

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,51 +1,101 @@
-use tiny_http::{Response, Server};
 use core::read_xml_dir_and_write;
+use std::error::Error;
+use std::fs::File;
+use tiny_http::{Header, Method, Request, Response, Server, StatusCode};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut args=std::env::args();
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut args = std::env::args();
     args.next();
-    let subcommand=args.next().unwrap_or_else(|| "index".to_string());
+    let subcommand = args.next().unwrap_or_else(|| "index".to_string());
 
     match subcommand.as_str() {
         "index" => {
-            let index_path=args.next().ok_or_else(||usage_and_error(subcommand.as_str()))?;
-            let target_path=args.next().unwrap_or_else(||String::from("index.json"));
+            let index_path = args
+                .next()
+                .ok_or_else(|| usage_and_error(subcommand.as_str()))?;
+            let target_path = args.next().unwrap_or_else(|| String::from("index.json"));
             read_xml_dir_and_write(index_path, target_path)?;
         }
-        "search"=>{
+        "search" => {
             todo!("search")
         }
-        "serve"=>{
-            let address=args.next().unwrap_or("127.0.0.1:8080".to_string());
-            let server=Server::http(&address).map_err(|_|{
+        "serve" => {
+            let address = args.next().unwrap_or("127.0.0.1:8080".to_string());
+            let server = Server::http(&address).map_err(|_| {
                 eprintln!("ERROR: unable to start server: {}", address);
                 std::process::exit(1);
             })?;
-            println!("Listening on http://{}", address);
+            println!("Listening on http://{address}");
             for req in server.incoming_requests() {
-                println!("INFO: received request! method: {:?}, url: {:?}",req.method(), req.url());
-                let resp=Response::from_string("hello");
-                req.respond(resp).unwrap_or_else(|err|{
-                    eprintln!("ERROR: failed to respond to response: {}", err);
-                });
+                serve_request(req)?;
             }
-            todo!("serve")
         }
-        _=>{
-
+        _ => {
+            usage_and_error(subcommand.as_str());
         }
-    }
-
-    for file_path in std::env::args().skip(1).take(1) {
-        read_xml_dir_and_write(file_path, "index.json")?;
     }
     Ok(())
 }
-fn usage_and_error(subcommand: &str)->Box<dyn std::error::Error>{
+
+fn serve_request(req: Request) -> Result<(), Box<dyn Error>> {
+    println!(
+        "INFO: received request! method: {:?}, url: {:?}",
+        req.method(),
+        req.url()
+    );
+    match req.method() {
+        Method::Get => match req.url() {
+            "/" | "/index.html" => {
+                serve_static_file(req, "crates/app/resources/index.html")
+                    .expect("Unable to serve static files:{index.html}");
+            }
+            "/index.js" => {
+                serve_static_file(req, "crates/app/resources/index.js")
+                    .expect("Unable to serve static files:js");
+            }
+            _ => req
+                .respond(Response::new_empty(StatusCode(404)))
+                .unwrap_or_else(|err| {
+                    eprintln!("ERROR: failed to respond: {}", err);
+                }),
+        },
+        Method::Post =>{
+            match req.url() {
+                "/api/search" =>{
+
+                }
+                _ => todo!()
+            }
+        }
+        _ => todo!(),
+    }
+    Ok(())
+}
+fn serve_static_file(req: Request, file_path: &str) -> Result<(), Box<dyn Error>> {
+    let file_suffix = file_path
+        .split_at(file_path.find('.').and_then(|i| Some(i + 1)).unwrap_or(0))
+        .1;
+    let resp = Response::from_file(File::open(file_path)?).with_header(
+        Header::from_bytes(
+            "Content-Type",
+            format!("text/{}; charset=utf-8", file_suffix),
+        )
+        .expect("So we didn't put any garbage in the headers"),
+    );
+    req.respond(resp).unwrap_or_else(|err| {
+        eprintln!("ERROR: failed to respond to response: {}", err);
+    });
+    Ok(())
+}
+fn usage_and_error(subcommand: &str) -> Box<dyn Error> {
     println!("Usage: [subcommand] [arg] [options]");
-    println!("subcommand index: index for the next arg as the input file and the second arg for output file if there is.Default it will output to 'index.json'");
+    println!(
+        "subcommand index: index for the next arg as the input file and the second arg for output file if there is.Default it will output to 'index.json'"
+    );
     println!("subcommand search: search the current directory if it does not exist");
-    println!("subcommand serve: init the http server for net,you can add a optional[arg] for the address");
+    println!(
+        "subcommand serve: init the http server for net,you can add a optional[arg] for the address"
+    );
     println!();
     eprintln!("ERROR: unknown subcommand: {subcommand}");
     std::process::exit(1);
